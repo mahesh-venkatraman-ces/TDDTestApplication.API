2a0b12e95a12bf254df93814168e98c5633ac705
Mahesh Venkatraman <mahesh.venkatraman@cesltd.com>
2023-10-13 20:37:24 +0530

    Integration Test Added

---
+++ TDDTestApplication.IntegrationTest/Helpers/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Data.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+
+namespace TDDTestApplication.IntegrationTest.Helpers
+{
+    public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> where TProgram : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {               
+                services.AddSingleton<DbConnection>(container =>
+                {
+                    var connection = new SqliteConnection("DataSource=:memory:");
+                    connection.Open();
+
+                    return connection;
+                });
+
+                services.AddDbContext<ApplicationDbContext>((container, options) =>
+                {
+                    var connection = container.GetRequiredService<DbConnection>();
+                    options.UseSqlite(connection);
+                });
+            });
+        }
+    }
+}

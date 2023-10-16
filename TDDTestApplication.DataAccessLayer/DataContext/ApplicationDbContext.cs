adc03289e5416e75af7cbc616565479111ed9403
Mahesh Venkatraman <mahesh.venkatraman@cesltd.com>
2023-10-16 16:09:30 +0530

    code clean up, error fixes

--- TDDTestApplication.DataAccessLayer/DataContext/ApplicationDbContext.cs
+++ TDDTestApplication.DataAccessLayer/DataContext/ApplicationDbContext.cs
@@ -12,14 +12,7 @@ namespace TDDTestApplication.DataAccessLayer.DataContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
-                 new User
-                 {
-                     UserId = 1,
-                     Username = "Mahesh",
-                     Password = "Mahesh123",
-                     Name = "Mahesh",
-                     Surname = "Venkatraman",
-                 }
+                 new User()
              );
         }
     }
